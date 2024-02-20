e10f77f63fb78984b751b620ad411eb6b25480c2
Zappa.20201 <Zappa.20201@E2-9-PC13>
2024-02-20 13:07:00 +0100

    Aggiungere i file di progetto.

---
+++ prioritaria/CodaPrioritaria.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prioritaria
+{
+	/*
+	Inserimento di un elemento (mantenendo l'ordinamento)
+	Ricerca di un elemento
+	Lettura del primo elemento(Peek)
+	Estrazione del primo elemento
+	Estrazione dell'ultimo elemento
+	IsEmpty()
+	*/
+	public class CodaPrioritaria : IComparable
+	{
+		private ArrayList _ar;
+		public ArrayList Ar
+		{
+			get { return _ar; }
+			set { _ar = value; }
+		}
+
+		public CodaPrioritaria()
+		{
+			Ar = new ArrayList();
+		}
+		public CodaPrioritaria(ArrayList ar)
+		{
+			Ar = ar;
+		}
+
+		public CodaPrioritaria(CodaPrioritaria cp)
+		{
+			Ar = cp.Ar;
+		}
+
+		public void Aggiunta(object value)
+		{
+			Ar.Add(value);
+		}
+
+		public int Ricerca(object value)
+		{
+			return Ar.IndexOf(value);
+		}
+
+		public object Peek()
+		{
+			if (IsEmpty())
+			{
+				return null;
+			} 
+			
+			return Ar[0];
+		}
+
+		public bool IsEmpty()
+		{
+			return Ar.Count == 0;
+		}
+
+		public object EstraiUltimo()
+		{
+			if (IsEmpty())
+			{
+				return null;
+			}
+			int ind = Ar.Count - 1;
+			object value = Ar[ind];
+			Ar.RemoveAt(ind);
+			return value;
+		}
+
+		public object EstraiPrimo()
+		{
+			if (IsEmpty())
+			{
+				return null;
+			}
+			
+			object value = Ar[0];
+			Ar.RemoveAt(0);
+			return value;
+		}
+
+		public override bool Equals(object obj)
+		{
+			if(!(obj is CodaPrioritaria) || obj == null)
+			{
+				return false;
+			}
+			CodaPrioritaria altro = (CodaPrioritaria)obj;
+			return Ar.Equals(altro.Ar);
+		}
+
+		public int CompareTo(object obj)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ prioritaria/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prioritaria
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			CodaPrioritaria cp = new CodaPrioritaria();
+			int scelta = 0;
+			do
+			{
+				Console.Clear();
+				Console.WriteLine("1 - Inserisci elemento");
+				Console.WriteLine("2 - Ricerca elemento");
+				Console.WriteLine("3 - Lettura primo elemento");
+				Console.WriteLine("4 - Estrazione primo elemento");
+				Console.WriteLine("5 - Estrazione ultimo elemento");
+				Console.WriteLine("6 - Controllo se Ar è vuota");
+				Console.WriteLine("7 - Visualizza");
+				Console.WriteLine("0 - Esci");
+				Console.WriteLine("Inserisci la scelta:");
+				scelta = int.Parse(Console.ReadLine());
+
+				switch (scelta)
+				{
+					case 0:
+						break;
+					case 1:
+						Console.WriteLine("Inserisci elemento");
+						object value = Console.ReadLine();
+						cp.Aggiunta(value);
+						Console.WriteLine("Elemento aggiunto");
+						break;
+						case 2:
+						Console.WriteLine("Inserisci elemento da ricercare");
+						object cerca = Console.ReadLine();
+						int ind = cp.Ricerca(cerca);
+						Console.WriteLine($"Elemento trovato in posizione {ind}");
+						break;
+					case 3:
+						object peek = cp.Peek();
+						Console.WriteLine($"Peek: {peek}");
+						break; 
+					case 4:
+						object primo = cp.EstraiPrimo();
+						Console.WriteLine($"Primo: {primo}");
+						break;
+						case 5:
+						object ultimo = cp.EstraiUltimo();
+						Console.WriteLine($"Primo: {ultimo}");
+						break;
+						case 6:
+						bool controllo = cp.IsEmpty();
+						Console.WriteLine("La lista è vuota? " + controllo);
+						break;
+						case 7:
+						foreach(var item in cp.Ar)
+						{
+							Console.WriteLine(item);
+						}
+						break;
+				}
+				Console.ReadLine();
+			} while (scelta != 0);
+
+		}
+	}
+}
